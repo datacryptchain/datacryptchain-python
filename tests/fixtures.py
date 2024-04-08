42fe7e8e29e5cfb3c1ff57153ad74d2c40d14c4f
Jeffrey Franc <jeffrey.franc@gmail.com>
2024-04-08 17:18:31 +0000

    Adds test for ledger validity

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -8,3 +8,13 @@ beamer,white,25,54
 raven,black,21,47
 </csv><comment>DataCryptChain Updated</comment><datetime>Mon Apr  8 16:49:45 2024 UTC</datetime><hash>3082035d548d72e25c99778a6d726556cfa23a8cf61a6116395eb8903ba650e4</hash></block></DataCryptChain>
 """
+
+INVALID_LEDGER = """
+<DataCryptChain><block><csv>Create Your New CSV Content Here</csv><comment>DataCryptChain Initialized</comment><datetime>Mon Apr  8 16:47:51 2024 UTC</datetime><hash>c347688cc7507f8fbb862879190fd89a2518a1ef99fbd87910d9a1ed29c98c10</hash></block><block><csv>name,color,height,weight
+monty,black,26,49
+molly,black,25,51
+beamer,white,25,55
+raven,black,21,47
+</csv><comment>DataCryptChain Updated</comment><datetime>Mon Apr  8 16:49:45 2024 UTC</datetime><hash>3082035d548d72e25c99778a6d726556cfa23a8cf61a6116395eb8903ba650e4</hash></block></DataCryptChain>
+"""
+
--- tests/test_epic_ledger.py
+++ tests/test_epic_ledger.py
@@ -9,11 +9,27 @@ class TestLedgerValid(fakeunittest.TestCase):
         self.setUpPyfakefs()
         
     def test_ledger_is_valid(self):
+        LEDGER_FILENAME = "poodles_valid.dcl"
         valid_ledger = fixtures.VALID_LEDGER
-        #errors = dcc.make_keys()
-        #self.assertEqual(errors, 0)
-        #self.assertTrue(os.path.exists("public.dcp"))
-        #self.assertTrue(os.path.exists("secret.dcs"))
+        with open(LEDGER_FILENAME, "w") as text_file:
+            text_file.write(valid_ledger)
+        errors = dcc.validate_ledger(LEDGER_FILENAME)
+        self.assertEqual(errors, 0)
+
+
+class TestLedgerInalid(fakeunittest.TestCase):
+    def setUp(self):
+        self.setUpPyfakefs()
+        
+    def test_ledger_is_invalid(self):
+        LEDGER_FILENAME = "poodles_invalid.dcl"
+        invalid_ledger = fixtures.INVALID_LEDGER
+        with open(LEDGER_FILENAME, "w") as text_file:
+            text_file.write(invalid_ledger)
+        errors = dcc.validate_ledger(LEDGER_FILENAME)
+        self.assertEqual(errors, 1)
+ 
+
         
         
 
